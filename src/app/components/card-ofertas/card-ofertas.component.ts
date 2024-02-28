6dffdee4d859ce141eb44e606421703db3c839ed
alejandrojurado28 <147718462+alejandrojurado28@users.noreply.github.com>
2024-02-28 13:10:05 +0100

    Implementando cartas y maquetando la web

---
+++ src/app/components/card-ofertas/card-ofertas.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-ofertas',
+  standalone: true,
+  imports: [],
+  templateUrl: './card-ofertas.component.html',
+  styleUrl: './card-ofertas.component.css'
+})
+export class CardOfertasComponent {
+
+}
--- src/app/views/home-view/home-view.component.ts
+++ src/app/views/home-view/home-view.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
 import { CarouselComponent } from '../../components/carousel/carousel.component';
+import { CardOfertasComponent } from '../../components/card-ofertas/card-ofertas.component';
 
 @Component({
   selector: 'app-home-view',
   standalone: true,
-  imports: [HeaderComponent, CarouselComponent],
+  imports: [HeaderComponent, CarouselComponent, CardOfertasComponent],
   templateUrl: './home-view.component.html',
   styleUrl: './home-view.component.css'
 })
