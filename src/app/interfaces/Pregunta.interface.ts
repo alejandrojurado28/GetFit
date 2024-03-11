f4b20a6db8805667e577c0bd68d3e4db4e404821
alejandrojurado28 <147718462+alejandrojurado28@users.noreply.github.com>
2024-03-11 17:35:38 +0100

    Implementando la creación de preguntas por parte de los usuarios.

--- src/app/interfaces/Pregunta.interface.ts
+++ src/app/interfaces/Pregunta.interface.ts
@@ -1,5 +1,6 @@
 export interface Pregunta {
     id: number,
+    usuario: string,
     asunto: string,
     pregunta: string
 }
--- src/app/views/about-view/about-view.component.ts
+++ src/app/views/about-view/about-view.component.ts
@@ -1,13 +1,64 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Pregunta } from '../../interfaces/Pregunta.interface';
+import { PreguntaService } from '../../services/pregunta.service';
+import { Usuario } from '../../interfaces/Usuario.interface';
 
 @Component({
   selector: 'app-about-view',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, CommonModule, FormsModule],
   templateUrl: './about-view.component.html',
   styleUrl: './about-view.component.css'
 })
-export class AboutViewComponent {
+export class AboutViewComponent implements OnInit {
+  preguntas?: Pregunta[];
+  nuevaPregunta: Pregunta = {
+    id: 0,
+    usuario: '',
+    asunto: '',
+    pregunta: ''
+  };
+
+  currentUser: Usuario | null;
+
+  constructor(private preguntaService: PreguntaService) {
+    const userString = localStorage.getItem('usuario');
+    this.currentUser = userString ? JSON.parse(userString) : null;
+  }
+
+  ngOnInit(): void {
+    this.fetchPreguntas();
+  }
+
+  private fetchPreguntas() {
+    this.preguntaService.findAll().subscribe({
+      next: value => {
+        this.preguntas = value;
+        console.log(value);
+      },
+      error: error => {console.log(error)}
+    })
+  }
+
+  createPregunta() {
+    this.preguntaService.create(this.nuevaPregunta).subscribe({
+      next: () => {
+        this.fetchPreguntas();
+        this.nuevaPregunta = {
+          id: 0,
+          usuario: '',
+          asunto: '',
+          pregunta: ''
+        };
+      },
+      error: error => {
+        console.error(error);
+      }
+    })
+  }
+
 
 }
